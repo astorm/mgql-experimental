65d2729433a3671428628c58f5cbb3abb85efbf3
Alan Storm <astorm@alanstorm.com>
2019-05-04 13:21:59 -0700

    PHP Function, plus everything actually part of object

--- tests/Magento/Framework/DataObject.test.js
+++ tests/Magento/Framework/DataObject.test.js
@@ -11,9 +11,14 @@ describe('Checking features on Magento\Framework\DataObject', function(){
 
     it("should allow users to exersize magic get/set functions", function(){
         var object = DataObject.factory();
-        var valueToTest = "foo's value"
-        object.setFoo(valueToTest);
-        object.getFoo().should.be.equal(valueToTest);
+        var valueToTest1 = "foo's value"
+        var valueToTest2 = "bar's value"
+
+        object.setFoo(valueToTest1);
+        object.setBar(valueToTest2);
+        object.getFoo().should.be.equal(valueToTest1);
+        object.getBar().should.be.equal(valueToTest2);
+        object.getFoo().should.be.equal(valueToTest1);
     });
 });
 
